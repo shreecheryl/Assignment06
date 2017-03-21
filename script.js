c7db69699ac0422f6068305d6e2412b068c9fd6a
shreecheryl <shreecheryl@att.net>
2017-03-21 14:42:29 -0700

    in progress 032117

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function init() {
     "use strict";
+    document.deliveryLocation.name.focus();
+    
+    function required(event) {
+        if (event.target.value === "") {
+            event.target.nextElementSibling.firstChild.nodeValue = "Required field";
+            event.target.focus();
+        } else {
+            event.target.nextElementSibling.firstChild.nodeValue = "";
+        }
+    }
+    
+    function onlyLetters(event) {
+        var str = event.target.value, patt = /[0-9]/g;
+        if (patt.test(str)) {
+            event.target.nextElementSibling.firstChild.nodeValue = "This field can only contain letters";
+            event.target.focus();
+        }
+    }
+    
+    // Check name entry
+    
+    var customerName = document.deliveryLocation.name;
+    customerName.addEventListener("blur", required);
+    customerName.addEventListener("blur", onlyLetters);
     
     // Display input field if they select "other" for Address Type
     
