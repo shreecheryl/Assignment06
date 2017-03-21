087f33e7afaca5c50304c945cf4fdc8b3ca42ed8
shreecheryl <shreecheryl@att.net>
2017-03-21 12:03:23 -0700

    in progress 032117

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function init() {
     "use strict";
-    var selection = document.getElementById("addressType");
+    
+    // Display input field if they select "other" for Address Type
+    
+    var selection = document.deliveryLocation.addressType; // alternative syntax to document.getElementById("addressType") using form name attribute
     selection.addEventListener("change", function () {
         if (document.getElementById("other").selected) {
             var other = document.getElementById("addressType2");
-            other.setAttribute("class", "form-group");
-            document.getElementById("otherAddressType").focus();
+            other.setAttribute("class", "form-group indent");
+            document.deliveryLocation.otherAddressType.focus();
         }     
     });
-    
+      
 } // end of init
 
 window.addEventListener("load", init);
