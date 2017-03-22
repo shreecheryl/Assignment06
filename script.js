fab3afe69e3a5fe837dfe4a52021bbbd4cfdd5d3
shreecheryl <shreecheryl@att.net>
2017-03-22 10:01:53 -0700

    in progress 032217

--- script.js
+++ script.js
@@ -19,11 +19,66 @@ function init() {
         }
     }
     
-    // Check name entry
+    function stateCheck(event) {
+        var state = event.target.value, patt = /[A-Z]{2}/i;
+        if (!patt.test(state)) {
+            event.target.nextElementSibling.firstChild.nodeValue = "Invalid entry";
+            event.target.value = "";
+            event.target.focus();
+        } else {
+            event.target.value = event.target.value.toUpperCase();
+            event.target.nextElementSibling.firstChild.nodeValue = "";
+        }
+    }
+    
+    function checkAddressType() {
+        var chosenOption = document.deliveryLocation.addressType.selectedIndex;
+        var addressType = document.deliveryLocation.addressType.options[chosenOption].value;
+        if (addressType === "select") {
+            event.target.nextElementSibling.firstChild.nodeValue = "Please select an option";
+            event.target.focus();  
+        } else if (addressType === "other") {
+            event.target.nextElementSibling.firstChild.nodeValue = "";
+            document.deliveryLocation.otherAddressType.focus();
+        } else {
+            event.target.nextElementSibling.firstChild.nodeValue = "";
+            document.deliveryLocation.address.focus();
+        }
+    }
+    
+    function checkEntry(event) {
+        var patt;
+        switch (event.target.name) {
+            case "zipCode":
+                patt = /^[0-9]{5}(?:-[0-9]{4})?$/;
+                break;
+            case "phone":
+                patt = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/;
+                break;
+            case "email":
+                patt = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+                break;
+        }
+        if (!patt.test(event.target.value)) {
+            event.target.nextElementSibling.firstChild.nodeValue = "Invalid entry";
+            event.target.value = "";
+            event.target.focus();
+        } else {
+            event.target.nextElementSibling.firstChild.nodeValue = "";
+            console.log(event.target);
+        }
+    }
+    
+    // Check Name Entry
     
     var customerName = document.deliveryLocation.name;
     customerName.addEventListener("blur", required);
     customerName.addEventListener("blur", onlyLetters);
+    
+    // Check Address Type
+    var addressType = document.deliveryLocation.addressType;
+    addressType.addEventListener("blur", checkAddressType);
+    addressType.addEventListener("change", checkAddressType);
     
     // Display input field if they select "other" for Address Type
     
@@ -35,7 +90,41 @@ function init() {
             document.deliveryLocation.otherAddressType.focus();
         }     
     });
-      
+    
+    // Check Specify Address Type Entry
+    var otherAddressType = document.deliveryLocation.otherAddressType;
+    otherAddressType.addEventListener("blur", required);
+    
+    // Check Address Entry
+    
+    var address = document.deliveryLocation.address;
+    address.addEventListener("blur", required);
+    
+    // Check City Entry
+    
+    var city = document.deliveryLocation.city;
+    city.addEventListener("blur", required);
+    
+    // Check State Entry
+    
+    var state = document.deliveryLocation.state;
+    state.addEventListener("blur", stateCheck);
+    
+    // Check Zip Code Entry
+    
+    var zip = document.deliveryLocation.zipCode;
+    zip.addEventListener("blur", checkEntry);           
+    
+    // Check Phone Number Entry
+    
+    var phone = document.deliveryLocation.phone;
+    phone.addEventListener("blur", checkEntry);
+                
+    // Check Email Entry
+    
+    var email = document.deliveryLocation.email;
+    email.addEventListener("blur", checkEntry); 
+                
 } // end of init
 
 window.addEventListener("load", init);
